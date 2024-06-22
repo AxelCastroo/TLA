e3e706cdf5ec884095aab8d3345bb671577c0c71
sant9s <sgalarraga@itba.edu.ar>
2024-06-22 09:36:41 -0300

    created EXPTree.java, doesnt do anything yet

---
+++ src/main/c/backend/domain-specific/src/main/java/EXPTree.java
@@ -0,0 +1,57 @@
+import java.util.Stack;
+
+public class EXPTree {
+    private Node<String> root;
+
+    public EXPTree(String expression) {
+        root = constructTree(expression);
+    }
+
+    private Node<String> constructTree(String expression) {
+        Stack<Node<String>> stack = new Stack<>();
+        for (String token : expression.split(" ")) {
+            if (isOperator(token)) {
+                Node<String> node = new Node<>(token);
+                node.setRight(stack.pop());
+                node.setLeft(stack.pop());
+                stack.push(node);
+            } else {
+                stack.push(new Node<>(token));
+            }
+        }
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    public int calculate() {
+        return evaluate(root);
+    }
+
+    private int evaluate(Node<String> node) {
+        if (node == null) {
+            return 0;
+        }
+
+        if (!isOperator(node.getData())) {
+            return Integer.parseInt(node.getData());
+        }
+
+        int leftValue = evaluate(node.getLeft());
+        int rightValue = evaluate(node.getRight());
+
+        switch (node.getData()) {
+            case "+":
+                return leftValue + rightValue;
+            case "-":
+                return leftValue - rightValue;
+            case "*":
+                return leftValue * rightValue;
+            case "/":
+                return leftValue / rightValue;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + node.getData());
+        }
+    }
